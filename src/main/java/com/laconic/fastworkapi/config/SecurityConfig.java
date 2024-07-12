ade27d208cf9ffca40f17782e1913c48f6173426
Srijak <srijakd@gmail.com>
2024-07-12 17:44:03 +0700

    Update security config

--- src/main/java/com/laconic/fastworkapi/config/SecurityConfig.java
+++ src/main/java/com/laconic/fastworkapi/config/SecurityConfig.java
@@ -4,30 +4,51 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    String issuerUri;
+    @Value("${jwt.public-key}")
+    private String publicKey;
 
     @Bean
-    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+    public JwtDecoder jwtDecoder() throws Exception {
+        RSAPublicKey rsaPublicKey = convertToPublicKey(publicKey);
+        return NimbusJwtDecoder.withPublicKey(rsaPublicKey).build();
+    }
 
-        return http
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/actuator/**",
                                         "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2ResourceServer ->
-                        oauth2ResourceServer
-                                .jwt(jwt ->
-                                        jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
-                                )
-                ).build();
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
+
+    private RSAPublicKey convertToPublicKey(String publicKey) throws Exception {
+        publicKey = publicKey.replace("-----BEGIN PUBLIC KEY-----", "")
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s+", "");
+        byte[] keyBytes = Base64.getDecoder().decode(publicKey);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return (RSAPublicKey) keyFactory.generatePublic(spec);
     }
 }
