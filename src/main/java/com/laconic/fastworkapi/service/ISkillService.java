2a8dda120e350c890c94d7bc17be13bc8af8d767
Srijak <srijakd@gmail.com>
2024-07-16 12:09:35 +0700

    Add API endpoint for adding skills to user

--- src/main/java/com/laconic/fastworkapi/service/ISkillService.java
+++ src/main/java/com/laconic/fastworkapi/service/ISkillService.java
@@ -6,12 +6,19 @@ import com.laconic.fastworkapi.dto.pagination.PaginationDTO;
 import com.laconic.fastworkapi.dto.pagination.SearchAndFilterDTO;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public interface ISkillService {
     SkillDTO save(SkillDTO skillDTO);
+
     void delete(UUID id);
+
     SkillDTO update(UUID id, SkillDTO skillDTO);
+
     List<SkillDTO> getAll();
+
     PaginationDTO<SkillDTO> getAll(PageAndFilterDTO<SearchAndFilterDTO> pageAndFilterDTO);
+
+    List<SkillDTO> addSkills(Long profileId, Set<UUID> skillIds);
 }
--- src/main/java/com/laconic/fastworkapi/service/impl/SkillService.java
+++ src/main/java/com/laconic/fastworkapi/service/impl/SkillService.java
@@ -1,16 +1,17 @@
 package com.laconic.fastworkapi.service.impl;
 
 import com.laconic.fastworkapi.constants.AppMessage;
-import com.laconic.fastworkapi.dto.ProfileDTO;
 import com.laconic.fastworkapi.dto.SkillDTO;
 import com.laconic.fastworkapi.dto.pagination.PageAndFilterDTO;
 import com.laconic.fastworkapi.dto.pagination.PaginationDTO;
 import com.laconic.fastworkapi.dto.pagination.SearchAndFilterDTO;
-import com.laconic.fastworkapi.entity.Profile;
 import com.laconic.fastworkapi.entity.Skill;
+import com.laconic.fastworkapi.entity.UserSkill;
 import com.laconic.fastworkapi.helper.ExceptionHelper;
 import com.laconic.fastworkapi.helper.PaginationHelper;
 import com.laconic.fastworkapi.repo.ISkillRepo;
+import com.laconic.fastworkapi.repo.IUserRepo;
+import com.laconic.fastworkapi.repo.IUserSkillRepo;
 import com.laconic.fastworkapi.repo.specification.GenericSpecification;
 import com.laconic.fastworkapi.service.ISkillService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +21,19 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class SkillService implements ISkillService {
     private final ISkillRepo skillRepo;
+    private final IUserRepo userRepo;
+    private final IUserSkillRepo userSkillRepo;
 
     @Autowired
-    public SkillService(ISkillRepo skillRepo) {
+    public SkillService(ISkillRepo skillRepo, IUserRepo userRepo, IUserSkillRepo userSkillRepo) {
         this.skillRepo = skillRepo;
+        this.userRepo = userRepo;
+        this.userSkillRepo = userSkillRepo;
     }
 
     @Override
@@ -60,7 +66,30 @@ public class SkillService implements ISkillService {
                         "name"));
         var result = this.skillRepo.findAll(specification, pageAndFilterDTO.getPageRequest());
         return PaginationHelper.getResponse(result,
-                                            result.getContent().stream().map(SkillDTO::new).toList());
+                result.getContent().stream().map(SkillDTO::new).toList());
+    }
+
+    @Override
+    public List<SkillDTO> addSkills(Long profileId, Set<UUID> skillIds) {
+        var profile =
+                this.userRepo.findById(profileId)
+                        .orElseThrow(ExceptionHelper.throwNotFoundException(AppMessage.USER, "id",
+                                profileId.toString()));
+
+        var userSkills = skillIds.stream().map(skillId -> UserSkill.builder()
+                .skillId(skillId)
+                .userId(profileId)
+                .build()).collect(Collectors.toList());
+
+        this.userSkillRepo.saveAll(userSkills);
+
+        var savedSkillIds = this.userSkillRepo.findAllByUserId(profileId).stream()
+                .map(UserSkill::getSkillId)
+                .collect(Collectors.toSet());
+
+        return this.skillRepo.findAllById(savedSkillIds).stream()
+                .map(skill -> new SkillDTO(skill.getId(), skill.getName()))
+                .collect(Collectors.toList());
     }
 
     private Skill getSkill(UUID id) {
