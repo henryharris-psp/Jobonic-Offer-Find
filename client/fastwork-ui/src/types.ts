9932b50c3ab59eba6924722d70ccd200a27c1e0c
Emmy <hmy1504@gmail.com>
2024-08-02 16:07:48 +0700

    edit service fetching (in progress) in ServiceMatchCard and declare types

--- client/fastwork-ui/src/types.ts
+++ client/fastwork-ui/src/types.ts
@@ -1,29 +1,94 @@
-export interface Service {
-    id: string;
-    name: string;
-    image: string;
-    bullet1: string;
-    bullet2: string;
-    bullet3: string;
-    rating: number;
-    reviews: number;
-    price: string;
-    description: string;
-    reviewsDetail: { reviewer: string; comment: string; rating: number }[];
-    numSold: number;
-} 
-// export interface JobData {
-//     title: string;
-//     company: string;
-//     work_category: string;
-//     employment_type: string;
-//     description_1: string;
-//     description_2: string;
-//     description_3: string;
-//     examples_of_work: string;
-//     submission_deadline: Date;
-//     budget: number;
-//     language: string
-//     location: string;
-//     days_left: number
+// export interface Service {
+//     id: string;
+//     name: string;
+//     image: string;
+//     bullet1: string;
+//     bullet2: string;
+//     bullet3: string;
+//     rating: number;
+//     reviews: number;
+//     price: string;
+//     description: string;
+//     reviewsDetail: { reviewer: string; comment: string; rating: number }[];
+//     numSold: number;
 // }
+
+export interface User {
+    id: number,
+    username: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    companyName: string,
+    phoneNumber: string,
+    address: string,
+    image: string,
+    cardNumber: string,
+    cardExpiryDate: string,
+    walletAddress: string,
+    // number of reviewers
+    numReviews: number,
+    userExperienceList: string[],
+    userEducationList: string[],
+    skills: string[],
+    userId: number,
+    // add number of services sold in backend
+    numSold?: number,
+    // add rating in backend
+    rating?: number,
+}
+
+export interface ServiceRequestDTO {
+    id: string;
+    submissionDeadline: string;
+    workExample: string;
+};
+  
+export interface Service {
+    id: string,
+    serviceOfferDTO: any[],
+    serviceRequestDTO: {
+      id: string,
+      submissionDeadline: string,
+      workExample: string
+    },
+    profileDTO: {
+      id: number,
+      username: string,
+      firstName: string,
+      lastName: string,
+      email: string,
+      companyName: string,
+      phoneNumber: string,
+      address: string,
+      image: string,
+      cardNumber: string,
+      cardExpiryDate: string,
+      walletAddress: string,
+      numReviews: number,
+      userExperienceList: [],
+      userEducationList: [],
+      skills: string[],
+      userId: number,
+      // add number of services sold in backend
+      numSold?: number,
+      // add rating in backend
+      rating?: number,
+    },
+    title: string,
+    employmentType: string,
+    description: string,
+    description1: string,
+    description2: string,
+    description3: string,
+    languageSpoken: string,
+    location: string,
+    categoryDTO: {
+      id: string,
+      name: string
+    },
+    price: number,
+    priceUnit: string
+    // remove after implementing reviews table
+    reviewsDetail?: { reviewer: string; comment: string; rating: number }[];
+}
