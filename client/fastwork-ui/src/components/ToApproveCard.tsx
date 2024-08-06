2850bfc9ac68ce547031928304859aa864ade291
tayxuenye <tayxuenye@gmail.com>
2024-08-06 15:57:12 +0700

    edited myDashboard to match with chat statuses

--- client/fastwork-ui/src/components/ToApproveCard.tsx
+++ client/fastwork-ui/src/components/ToApproveCard.tsx
@@ -3,14 +3,14 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 
-interface CurrentlyEngagedCardProps {
+interface ToApproveCard {
   title: string;
   earned: string;
   description: { avatar: string; username: string; review: string }[];
   details: string[];
 }
 
-const CurrentlyEngagedOfferServicesCard: React.FC<CurrentlyEngagedCardProps> = ({ title, earned, description, details }) => {
+const ToApproveCard: React.FC<ToApproveCard> = ({ title, earned, description, details }) => {
   const router = useRouter();
 
   const handleChatClick = (e: React.MouseEvent) => {
@@ -27,7 +27,7 @@ const CurrentlyEngagedOfferServicesCard: React.FC<CurrentlyEngagedCardProps> = (
           alt={description[0].username}
         />
         <div className="text-black text-sm">
-          <span className="font-bold">{description[0].username}</span> is looking for
+          <span className="font-bold">{description[0].username}</span> hired you for
         </div>
       </div>
       <h3 className="font-bold text-xl mb-2">{title}</h3>
@@ -42,19 +42,12 @@ const CurrentlyEngagedOfferServicesCard: React.FC<CurrentlyEngagedCardProps> = (
             <path fillRule="evenodd" d="M3 5.983C3 4.888 3.895 4 5 4h14c1.105 0 2 .888 2 1.983v8.923a1.992 1.992 0 0 1-2 1.983h-6.6l-2.867 2.7c-.955.899-2.533.228-2.533-1.08v-1.62H5c-1.105 0-2-.888-2-1.983V5.983Zm5.706 3.809a1 1 0 1 0-1.412 1.417 1 1 0 1 0 1.412-1.417Zm2.585.002a1 1 0 1 1 .003 1.414 1 1 0 0 1-.003-1.414Zm5.415-.002a1 1 0 1 0-1.412 1.417 1 1 0 1 0 1.412-1.417Z" clipRule="evenodd"/>
           </svg>
         </button>
-        {/* <button className="ml-2 px-4 py-2 bg-[#0B2147] text-white rounded-lg font-semibold hover:bg-[#D0693B]">
-          Submit File
-        </button> */}
-        <input type="file" id="file-input" className="hidden" />
-          <label htmlFor="file-input" className="file-input-label ml-2 px-4 py-2 bg-[#0B2147] text-white rounded-lg font-semibold hover:bg-[#D0693B] cursor-pointer">
-            Submit File
-          </label>
       </div>
     </div>
   );
 };
 
-export default CurrentlyEngagedOfferServicesCard;
+export default ToApproveCard;
 
 
 
