0707277edeb3a20f2578316021f3d24009779a72
Lin Htin Nyein <lynnhtinnyein@gmail.com>
2024-08-17 17:11:19 +0700

    #0018019 replace dummy user data with actual data when authenticate.

--- client/fastwork-ui/src/types/users.ts
+++ client/fastwork-ui/src/types/users.ts
@@ -1,24 +1,39 @@
-export interface User {
-    id: number,
-    username: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    companyName: string,
-    phoneNumber: string,
-    address: string,
-    image: string,
-    cardNumber: string,
-    cardExpiryDate: string,
-    walletAddress: string,
+export interface Profile {
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    companyName: string;
+    phoneNumber: string;
+    address: string;
+    image: string;
+    cardNumber: string;
+    cardExpiryDate: string;
+    walletAddress: string;
     // number of reviewers
-    numReviews: number,
-    userExperienceList: string[],
-    userEducationList: string[],
-    skills: string[],
-    userId: number,
+    numReviews: number;
+    userExperienceList: string[];
+    userEducationList: string[];
+    skills: string[];
+    userId: number;
     // add number of services sold in backend
-    numSold?: number,
+    numSold?: number;
     // add rating in backend
-    rating?: number,
+    rating?: number;
+}
+
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    name_english: string | null;
+    name_local: string | null;
+    email: string;
+    sso_id: string;
+    image_url: string | null;
+    last_login_date: string | null,
+    roles: any,
+    profile: Profile
 }
