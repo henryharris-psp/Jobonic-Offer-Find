6ca23615ad1dffef2e192f276d25eb8ff47e09bc
Srijak <srijakd@gmail.com>
2024-08-19 15:11:44 +0700

    #0018029: My Profile

--- src/main/java/com/laconic/fastworkapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/laconic/fastworkapi/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.laconic.fastworkapi.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import oracle.jdbc.OracleDatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -24,6 +26,22 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleDocumentException(RuntimeException ex, WebRequest request) {
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.PAYLOAD_TOO_LARGE);
         return getProblemDetail(ex, request, problemDetail);
+    }
+
+    @ExceptionHandler(OracleDatabaseException.class)
+    public ResponseEntity<ProblemDetail> handleOracleDatabaseException(OracleDatabaseException ex, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+        problemDetail.setType(URI.create("DatabaseError"));
+
+        String errorMessage = ex.getMessage();
+        if (errorMessage.contains("ORA-00001: unique constraint")) {
+            problemDetail.setProperty("message", "A record with this unique constraint already exists.");
+        } else {
+            problemDetail.setProperty("message", "An unexpected database error occurred.");
+        }
+        LOGGER.debug("[{}] exception thrown. [{}]. Additional Info: [{}]", ex.getClass().getSimpleName(),
+                ex.getMessage(), request.getDescription(true));
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
     }
 
     private ProblemDetail getProblemDetail(Exception ex, WebRequest request, ProblemDetail problemDetail) {
