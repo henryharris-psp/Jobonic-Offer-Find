5484064250c1f6509aae2ad71f6ca3c885cf6b85
pyaesonephyo1996 <pyaesone1996phyo@gmail.com>
2024-09-19 09:04:42 +0700

    #0018623 New payment card for milestone and make new changes in progress list

--- client/fastwork-ui/src/components/chat/ProgressList/AlertMessage/FileAlertMessage.tsx
+++ client/fastwork-ui/src/components/chat/ProgressList/AlertMessage/FileAlertMessage.tsx
@@ -9,8 +9,8 @@ const FileSizeAlertModal: React.FC<FileSizeAlertModalProps> = ({ isOpen, onClose
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white rounded-lg shadow-lg p-6 w-[40%]">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 ">
+            <div className="bg-white rounded-lg shadow-lg p-6 w-[40%] sm:w-[80%] md:w-[80%]">
                 <h2 className="text-xl font-bold text-center text-cyan-900 mb-8">File Size Alert</h2>
                 <p className="mt-2 text-center text-md mb-8">The selected file is greater than 10MB. Please choose another file.</p>
                 <div className="flex justify-center">
