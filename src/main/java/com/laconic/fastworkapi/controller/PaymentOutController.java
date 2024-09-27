b9dd534be57b7461fe2e358a62125d6317354287
bibek <bibekupreti9813@gmail.com>
2024-09-27 16:24:13 +0700

    0018682: Need to create payment API(Backend)

--- src/main/java/com/laconic/fastworkapi/controller/PaymentOutController.java
+++ src/main/java/com/laconic/fastworkapi/controller/PaymentOutController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/payment-agreement")
+@RequestMapping("/api/v1/payment-out")
 @RequiredArgsConstructor
 public class PaymentOutController {
 
